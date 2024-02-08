efabe7f9ea26b8e44193326f615cfd38f9f23435
jgarn6733 <143364247+jgarn6733@users.noreply.github.com>
2024-02-07 16:39:41 -0800

    Pong commit #1
    Committing the main files and the start of the game.

---
+++ Pong/Assets/Scripts/PaddleController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleController : MonoBehaviour
+{
+    public float moveSpeed = 3f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+    }
+    void FixedUpdate()
+    {
+        float leftPaddle = Input.GetAxis("LeftPaddle");
+        float rightPaddle = Input.GetAxis("RightPaddle");
+
+        Rigidbody leftRb = GameObject.Find("LeftPaddle").GetComponent<Rigidbody>();
+        Rigidbody rightRb = GameObject.Find("RightPaddle").GetComponent<Rigidbody>();
+
+        Vector3 leftForce = Vector3.forward * leftPaddle * moveSpeed;
+        Vector3 rightForce = Vector3.forward * rightPaddle * moveSpeed;
+
+        leftRb.AddForce(leftForce, ForceMode.Acceleration);
+        rightRb.AddForce(rightForce, ForceMode.Acceleration);
+    }
+}
