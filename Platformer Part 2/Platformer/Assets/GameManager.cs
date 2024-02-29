a34d736a3f314ed66ed4f09fab23a96185d41dab
jgarn6733 <143364247+jgarn6733@users.noreply.github.com>
2024-02-28 22:36:55 -0800

    Update GameManager.cs

--- Platformer Part 2/Platformer/Assets/GameManager.cs
+++ Platformer Part 2/Platformer/Assets/GameManager.cs
@@ -30,38 +30,7 @@ public class GameManager : MonoBehaviour
         }
         string timeStr = $"Time \n {intTime}";
         timerText.text = timeStr;
-        string coinsStr;
-
         
-
-
-        if (Input.GetMouseButtonDown(0))
-        {
-            Ray origin = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit raycastHit;
-            bool hit = Physics.Raycast(origin, out raycastHit, 100f);
-            if (hit)
-            {
-                Material material = raycastHit.collider.GetComponent<Material>();
-                if (material = brickMaterial)
-                {
-                    GameObject obj = GameObject.Find(raycastHit.collider.name);
-                    Object.Destroy(obj);
-                } else if (material == questionMaterial)
-                {
-                    coins++;
-                    if (coins > 10)
-                    {
-                        coinsStr = $"Coins x0{coins}";
-                    } else
-                    {
-                        coinsStr = $"Coins x{coins}";
-                    }
-                    Debug.Log(coinsStr);
-                    coinsText.text = coinsStr;
-                }
-            }
-        }
     }
 
     public static void addScore(TextMeshProUGUI scoreText)
